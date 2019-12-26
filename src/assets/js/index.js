0053615d502b01d5e4c8757009039a39e7c11f9b
Valery Juravlev <valera030219972010@gmail.com>
2019-12-26 21:56:19 +0300

    add like button

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,11 +4,13 @@ const calendar = require('../../blocks/calendar/calendar.js');
 const dropdown = require('../../blocks/dropdown/dropdown.js');
 const range = require('../../blocks/range/range.js');
 const slider = require('../../blocks/slider/slider.js');
+const like = require('../../blocks/like/like.js')
 
 $(document).ready(() => {
     calendar;
     dropdown;
     range;
     slider;
+    like;
 });
 
---
+++ src/blocks/like/like.js
@@ -0,0 +1,14 @@
+const like = function() {
+    $('.like__field').click((e) => {
+        let event = e.target;
+        let numb = $(event).next('.like__label').html();
+        if($(event).is(':checked')) {
+            $(event).next('.like__label').html(+(numb) + 1);
+        }else{
+            $(event).next('.like__label').html(+(numb) - 1);
+        }
+        
+    });
+}();
+
+module.exports = like;
